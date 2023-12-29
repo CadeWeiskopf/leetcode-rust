37ba82e05c0e7c49a69899a857739abe74646388
Cade Weiskopf <weiskopfcd@gmail.com>
2023-12-29 16:17:00 -0500

    palindrome i32 solution

--- palindrome.rs
+++ palindrome.rs
@@ -1,5 +1,20 @@
 impl Solution {
-    pub fn is_palindrome(x: i32) -> bool {
+    pub fn is_palindrome_i32(x: i32) -> bool {
+        if (x < 0) {
+            return false;
+        }
+
+        let mut original: i32 = x;
+        let mut reversed: i32 = 0;
+        while original > 0 {
+            reversed = (reversed * 10) + (original % 10);
+            original /= 10;
+        }
+
+        return x == reversed;
+    }
+    
+    pub fn is_palindrome_str(x: i32) -> bool {
         let xs = x.to_string();
         let xsIt = xs.chars();
         let xsRevIt = xs.chars().rev();
